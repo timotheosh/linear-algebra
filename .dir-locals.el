d7f71a91dd2263029cf6f2aae5c7a708814b78fc
Tim Hawes <tim.hawes@genesys.com>
2024-09-13 14:56:49 -0400

    Initial Commit

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((eval . (setenv "PATH" (concat (getenv "PATH") ":/home/thawes/.pyenv/versions/anaconda3-2024.02-1/bin")))
+         (eval . (setq exec-path (append exec-path '("/home/thawes/.pyenv/versions/anaconda3-2024.02-1/bin")))))))
+
